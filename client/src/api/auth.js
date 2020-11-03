b37793f92c7a022a9569b642d26ec603109217be
Mawaddah <itzolda@gmail.com>
2020-11-03 20:50:35 +0000

    Implement client login
    - Add login page using semantic UI components
    - Submit email/password as JSON to the pre-written authentication
      API.

---
+++ client/src/api/auth.js
@@ -0,0 +1,10 @@
+export const loginUser = (email, password) => {
+  const options = {
+    method: "POST",
+    body: JSON.stringify({ email: email, password: password }),
+    headers: {
+      "content-type": "application/json"
+    }
+  };
+  return fetch("/auth/login", options).then(res => res.json());
+};
