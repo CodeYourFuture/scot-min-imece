9ce329bcd96aca3b816c2b7d0e1c39b5f8fb8d1f
hacersk <hacersk41@gmail.com>
2020-11-04 00:54:46 +0000

    Remove redundant code

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import Home from "./components/Home";
@@ -6,26 +6,24 @@ import "semantic-ui-css/semantic.min.css";
 import About from "./components/About";
 import Status from "./components/Status";
 import { NavLink, BrowserRouter as Router, Route } from "react-router-dom";
-import { Menu, Container, Image, Input } from "semantic-ui-react";
+import { Menu, Container, Image } from "semantic-ui-react";
 
 const Routes = () => {
   return (
     <Router>
       <Menu inverted>
         <Container>
-          <Menu.Item header>
+          <Menu.Item>
             <Image
               size="small"
               src="http://www.maryhillintegration.org.uk/wp-content/uploads/2016/10/banner.jpg"
             />
           </Menu.Item>
-
-          <Menu.Item header as={NavLink} to="/" name="home" />
-          <Menu.Item header as={NavLink} to="/about" name="about" />
-          <Menu.Item header as={NavLink} to="/status" name="status" />
+          <Menu.Item as={NavLink} to="/" name="home" />
+          <Menu.Item as={NavLink} to="/about" name="about" />
+          <Menu.Item as={NavLink} to="/status" name="status" />
         </Container>
       </Menu>
-
       <div>
         <Route path="/" exact component={Home} />
         <Route path="/about/" component={About} />
