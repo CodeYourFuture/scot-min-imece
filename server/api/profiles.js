5cf9908908a72ff12171ee89b543df65a0e1e7ce
hacersk <hacersk41@gmail.com>
2020-11-06 20:32:31 +0000

    Create profile.js for server/api

---
+++ server/api/profiles.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+const usersDb = require("../services/database/profiles");
+
+
+router.get("/", (req, res) => {
+	usersDb
+		.getAllProfiles()
+		.then((data) => {
+			res.send(data);console.log(data);
+		})
+		.catch((err) => {
+			console.error(err);
+			res.send(500);
+		});
+});
+
+
+module.exports = router;
