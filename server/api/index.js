2744e0e0d1b04c303f9ed86d27f285c2e4299575
hacersk <hacersk41@gmail.com>
2020-11-06 20:40:17 +0000

    Add /profiles path to server-index.js

--- server/api/index.js
+++ server/api/index.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
+const profiles = require("./profiles");
 const users = require("./users");
 const status = require("./status");
 
+
+router.use("/profiles", profiles);
 router.use("/users", users);
 router.use("/status", status);
+
 
 module.exports = router;
