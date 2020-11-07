7e8021fbc905f3df76bbd5eba763f47423a54f01
hacersk <hacersk41@gmail.com>
2020-11-07 13:23:30 +0000

    Remove getProfiles() function

--- client/src/api/status.js
+++ client/src/api/status.js
@@ -5,7 +5,3 @@ export const getStatus = () => {
 export const getUsers = () => {
   return fetch("/api/users").then(res => res.json());
 };
-
-export const getProfiles = () => {
-  return fetch("/api/profiles").then(res => res.json());
-};
