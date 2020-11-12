dc1d3cac1d9358685bfe4ef75151e9ae98f7bb0f
hacersk <hacersk41@gmail.com>
2020-11-12 12:30:32 +0000

    Add search to filter profiles

--- client/src/components/Profiles.js
+++ client/src/components/Profiles.js
@@ -1,40 +1,83 @@
 import React, { useState, useEffect } from "react";
-import { Table } from "semantic-ui-react";
+import { Container, Message, Search, Table } from "semantic-ui-react";
 import { getProfiles } from "../api/profiles";
 
 const Profiles = () => {
   const [profiles, setProfiles] = useState([]);
+  const [searchInput, setSearchInput] = useState("");
+
+  const handleSearchChange = event => {
+    setSearchInput(event.target.value);
+  };
 
   useEffect(() => {
     getProfiles().then(response => {
-      setProfiles(response);
+      setProfiles(() => {
+        return !searchInput
+          ? response
+          : response.filter(
+              profile =>
+                profile.first_name
+                  .toLowerCase()
+                  .includes(searchInput.toLowerCase()) ||
+                profile.last_name
+                  .toLowerCase()
+                  .includes(searchInput.toLowerCase()) ||
+                profile.email
+                  .toLowerCase()
+                  .includes(searchInput.toLowerCase()) ||
+                (profile.address &&
+                  profile.address
+                    .toLowerCase()
+                    .includes(searchInput.toLowerCase())) ||
+                (profile.phone_number &&
+                  profile.phone_number
+                    .toLowerCase()
+                    .includes(searchInput.toLowerCase())) ||
+                (profile.occupation &&
+                  profile.occupation
+                    .toLowerCase()
+                    .includes(searchInput.toLowerCase()))
+            );
+      });
     });
-  }, []);
-  return !profiles.length ? (
-    <p>No Profiles!</p>
-  ) : (
-    <Table celled>
-      <Table.Header>
-        <Table.Row>
-          <Table.HeaderCell>#</Table.HeaderCell>
-          <Table.HeaderCell>Name</Table.HeaderCell>
-          <Table.HeaderCell>Phone</Table.HeaderCell>
-          <Table.HeaderCell>Email</Table.HeaderCell>
-        </Table.Row>
-      </Table.Header>
-      <Table.Body>
-        {profiles.map(profile => (
-          <Table.Row key={profile.id}>
-            <Table.Cell children={profile.id}></Table.Cell>
-            <Table.Cell
-              children={profile.first_name + " " + profile.last_name}
-            ></Table.Cell>
-            <Table.Cell children={profile.phone_number}></Table.Cell>
-            <Table.Cell children={profile.email}></Table.Cell>
-          </Table.Row>
-        ))}
-      </Table.Body>
-    </Table>
+  }, [searchInput]);
+
+  return (
+    <Container text>
+      <Search
+        style={{ margin: "30px 0px" }}
+        onSearchChange={handleSearchChange}
+        value={searchInput}
+        showNoResults={false}
+      />
+      {profiles.length === 0 ? (
+        <Message>No matching profiles found</Message>
+      ) : (
+        <Table celled collapsing>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell>#</Table.HeaderCell>
+              <Table.HeaderCell>Name</Table.HeaderCell>
+              <Table.HeaderCell>Phone</Table.HeaderCell>
+              <Table.HeaderCell>Email</Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+            {profiles.map(profile => (
+              <Table.Row key={profile.id}>
+                <Table.Cell children={profile.id}></Table.Cell>
+                <Table.Cell
+                  children={profile.first_name + " " + profile.last_name}
+                ></Table.Cell>
+                <Table.Cell children={profile.phone_number}></Table.Cell>
+                <Table.Cell children={profile.email}></Table.Cell>
+              </Table.Row>
+            ))}
+          </Table.Body>
+        </Table>
+      )}
+    </Container>
   );
 };
 
