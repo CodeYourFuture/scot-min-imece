b5a8c93372cd8898ff6791604fdc0266131c04f7
hacersk <hacersk41@gmail.com>
2020-11-12 12:33:46 +0000

    Change the query to get all columns

--- server/services/database/profiles.js
+++ server/services/database/profiles.js
@@ -3,7 +3,7 @@ const config = require("../../config");
 const pool = new Pool(config);
 
 const getAllProfiles = () => {
-	return pool.query("SELECT id, first_name, last_name, phone_number, email FROM profiles")
+	return pool.query("SELECT * FROM profiles")
 	.then((result) => result.rows)
 	.catch((error) => {
 		console.error(error);
