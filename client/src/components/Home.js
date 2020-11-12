d09ed8131733fe03f581e040a9705e89712bdd12
hacersk <hacersk41@gmail.com>
2020-11-12 13:12:21 +0000

    Change home page to better fit search box and results

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,15 +1,14 @@
 import React from "react";
 import "../styles/Home.css";
 import Profiles from "../components/Profiles";
+import { Container, Header } from "semantic-ui-react";
 
 const Home = () => {
   return (
-    <div>
-      <header className="home-header">
-        <h1>MIN - Imece</h1>
-        <Profiles />
-      </header>
-    </div>
+    <Container text style={{ marginTop: "20px" }}>
+      <Header as="h1">MIN - Imece</Header>
+      <Profiles />
+    </Container>
   );
 };
 
