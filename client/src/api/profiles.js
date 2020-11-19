97d6089bc2a846e40d96a041c1eaea30ff026603
hacersk <hacersk41@gmail.com>
2020-11-19 17:21:46 +0000

    Create getProfile() function in api/profile.js to fetch data by profile id

--- client/src/api/profiles.js
+++ client/src/api/profiles.js
@@ -1,3 +1,7 @@
 export const getProfiles = () => {
   return fetch("/api/profiles").then(res => res.json());
 };
+
+export const getProfile = profileId => {
+  return fetch(`/api/profiles/${profileId}`).then(res => res.json());
+};
