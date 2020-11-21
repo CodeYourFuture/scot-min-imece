668e3b34dfa7623b286aa10ebed5583f3e482399
hacersk <hacersk41@gmail.com>
2020-11-21 01:03:20 +0000

    Update the Profile Card and use template literals in /components/ViewProfile.js

--- client/src/components/ViewProfile.js
+++ client/src/components/ViewProfile.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Container, Button, Card } from "semantic-ui-react";
+import { Container, Button, Card, Divider, Icon } from "semantic-ui-react";
 import { getProfile } from "../api/profiles";
 import { useParams } from "react-router-dom";
 
@@ -11,22 +11,43 @@ const ViewProfile = () => {
   useEffect(() => {
     getProfile(profileId).then(response => {
       setProfile(response);
-      document.title =
-        response.first_name + " " + response.last_name + " Profile";
+      document.title = `${response.first_name} ${response.last_name}  Profile`;
     });
   }, [profileId]);
 
   return (
-    <Container text>
-      <Card color="black">
+    <Container text style={{ marginTop: "35px" }}>
+      <Card color="black" textAlign="center" style={{ width: "auto" }}>
         <Card.Content>
           <Card.Header>
-            {profile.first_name + " " + profile.last_name}
+            {`${profile.first_name} ${profile.last_name}`}
           </Card.Header>
-          <Card.Meta>{profile.email}</Card.Meta>
-          <Card.Description>{profile.address}</Card.Description>
+          <Divider></Divider>
+          <Card.Content>
+            <Card.Description>
+              <Icon name="mail" />
+              {profile.email}
+            </Card.Description>
+            <Card.Description>
+              <Icon name="phone" />
+              {profile.phone_number}
+            </Card.Description>
+            <Card.Description>
+              <Icon name="address card outline"></Icon>
+              {profile.address}
+            </Card.Description>
+          </Card.Content>
+          <Divider></Divider>
           <Card.Content extra>
-            {profile.first_name} is a good software developer
+            {profile.first_name}
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. It has
+              survived not only five centuries, but also the leap into
+              electronic typesetting, remaining essentially unchanged.
+            </p>
           </Card.Content>
         </Card.Content>
         <Card.Content extra>
