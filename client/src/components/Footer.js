0181ec15b606ad08c59fb75934e6c18695ff7c18
hacersk <hacersk41@gmail.com>
2021-01-19 00:27:27 +0000

    Remove border, change 'font-size' in Footer.js

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -13,12 +13,16 @@ const Footer = () => {
   return (
     <Segment
       textAlign="center"
-      style={{ backgroundColor: "#F7F083", color: "black" }}
+      style={{ backgroundColor: "#F7F083", fontSize: "16px", border: "none" }}
     >
-      <Container>
-        <Grid divided columns={3}>
+      <Container fluid>
+        <Grid columns={3}>
           <Grid.Column width={4}>
-            <Header as="h4" content="Min-Imece-Team" />
+            <Header
+              as="h4"
+              content="Developers"
+              style={{ color: "#000", fontSize: "large" }}
+            />
             <List>
               <List.Item>Samuel | Natalia | Mawaddah </List.Item>
               <List.Item>Fatma | Hacer</List.Item>
