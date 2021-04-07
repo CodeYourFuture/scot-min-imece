b8dae192822383af47f07b09527bce76652f4b3f
Luke Taylor <tekul.hs@gmail.com>
2021-04-07 19:58:40 +0100

    Restyle Footer without semantic-ui

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,51 +1,36 @@
 import React from "react";
 import CYFLogo from "../assets/cyf_brand.png";
-import {
-  Container,
-  Grid,
-  Image,
-  Segment,
-  List,
-  Header
-} from "semantic-ui-react";
 
 const Footer = () => {
   return (
-    <Segment
-      textAlign="center"
-      style={{ backgroundColor: "#F7F083", fontSize: "16px", border: "none" }}
-    >
-      <Container fluid>
-        <Grid columns={3}>
-          <Grid.Column width={4}>
-            <Header
-              as="h4"
-              content="Developers"
-              style={{ color: "#000", fontSize: "large" }}
-            />
-            <List>
-              <List.Item>Samuel | Natalia | Mawaddah </List.Item>
-              <List.Item>Fatma | Hacer</List.Item>
-            </List>
-          </Grid.Column>
-          <Grid.Column width={8}>
-            <p>
-              A dashboard for Maryhill Integration Network that staff can use to
-              better track, and support service users and volunteers created by
-              CodeYourFuture Students
-            </p>
-          </Grid.Column>
-          <Grid.Column width={4}>
-            <Image
-              src={CYFLogo}
-              as="a"
-              size="medium"
-              href="https://codeyourfuture.io"
-            />
-          </Grid.Column>
-        </Grid>
-      </Container>
-    </Segment>
+    <div className="flex justify-around px-5 py-3 mt-5 lg:max-h-24 bg-yellow-200">
+      <div className="w-4/12 py-2 text-center lg:w-3/12">
+        <h4 className="mb-1 font-bold">Developers</h4>
+        <ul className="flex flex-col lg:flex-row justify-between">
+          <li className="px-1">Fatma</li>
+          <li className="px-1">Hacer</li>
+          <li className="px-1">Mawaddah</li>
+          <li className="px-1">Nataliah</li>
+          <li className="px-1">Samuel</li>
+        </ul>
+      </div>
+      <div className="flex flex-col justify-center px-10">
+        <p className="text-center text-lg">
+          A dashboard for Maryhill Integration Network that staff can use to
+          better track, and support service users and volunteers created by
+          CodeYourFuture Students
+        </p>
+      </div>
+      <div className="flex w-4/12 lg:w-3/12">
+        <a className="flex justify-around" href="https://codeyourfuture.io">
+          <img
+            className="object-contain"
+            src={CYFLogo}
+            alt="Code Your Future Logo"
+          ></img>
+        </a>
+      </div>
+    </div>
   );
 };
 
