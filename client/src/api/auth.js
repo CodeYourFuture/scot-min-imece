f323ba8bb2023d8814799ec25de512e044c571aa
Natalia <n5@mail.ru>
2021-07-07 00:49:37 +0100

    #63 Login form should handle server error differently from failed login

--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -6,5 +6,13 @@ export const loginUser = (email, password) => {
       "content-type": "application/json"
     }
   };
-  return fetch("/auth/login", options).then(res => res.json());
+  return fetch("/auth/login", options).then(res => {
+    if (res.status >= 500) {
+      throw new Error(res.statusText);
+    }
+    if (res.status === 403) {
+      return {};
+    }
+    return res.json();
+  });
 };
