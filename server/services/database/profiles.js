a6252b77083fdccf32f4d333489a9c11dea0c7fb
costanzazanoletti <zanoletti.cos@gmail.com>
2021-08-18 22:36:57 +0200

    added transaction in createProfile function

--- server/services/database/profiles.js
+++ server/services/database/profiles.js
@@ -1,80 +1,120 @@
-const { Pool } = require("pg");
-const config = require("../../config");
+const { Pool } = require('pg');
+const config = require('../../config');
 const pool = new Pool(config);
-const format = require("pg-format");
+const format = require('pg-format');
 
 const getAllProfiles = () => {
-	return pool.query("select * from profiles").then((result) => result.rows);
+  return pool.query('select * from profiles').then((result) => result.rows);
 };
 
-const createProfile = (newProfile) => {
-	return pool
-		.query(
-			"INSERT INTO profiles ( first_name, last_name, date_of_birth, gender, email, address, phone_number, type, nationality_id) values ( $1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING id",
-			[
-				newProfile.firstname,
-				newProfile.lastname,
-				newProfile.dob,
-				newProfile.gender,
-				newProfile.email,
-				newProfile.address,
-				newProfile.phone,
-				newProfile.profile_type,
-				newProfile.nationality,
-			],
-		)
-		.then((result)=>{
-			let profileId=result.rows[0].id;
-			const values = newProfile.groups.map((groupId) => [profileId, groupId]);
+const createProfile = async (newProfile) => {
+  const client = await pool.connect();
+  try {
+    await client.query('BEGIN');
+    let result = await client.query(
+      'INSERT INTO profiles ( first_name, last_name, date_of_birth, gender, email, address, phone_number, type, nationality_id) values ( $1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING id',
+      [
+        newProfile.firstname,
+        newProfile.lastname,
+        newProfile.dob,
+        newProfile.gender,
+        newProfile.email,
+        newProfile.address,
+        newProfile.phone,
+        newProfile.profile_type,
+        newProfile.nationality,
+      ]
+    );
+    let profileId = result.rows[0].id;
+    const values = newProfile.groups.map((groupId) => [profileId, groupId]);
 
-			if(values.length > 0) {
-				let sql = format("INSERT INTO profile_group VALUES %L", values);
-				return pool.query(sql);
-			}
-		});
+    if (values.length > 0) {
+      let sql = format('INSERT INTO profile_group VALUES %L', values);
+      await client.query(sql);
+    }
+    await client.query('COMMIT');
+  } catch (e) {
+    await client.query('ROLLBACK');
+    throw e;
+  } finally {
+    client.release();
+  }
+};
+
+const createProfileOld = (newProfile) => {
+  return pool
+    .query(
+      'INSERT INTO profiles ( first_name, last_name, date_of_birth, gender, email, address, phone_number, type, nationality_id) values ( $1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING id',
+      [
+        newProfile.firstname,
+        newProfile.lastname,
+        newProfile.dob,
+        newProfile.gender,
+        newProfile.email,
+        newProfile.address,
+        newProfile.phone,
+        newProfile.profile_type,
+        newProfile.nationality,
+      ]
+    )
+    .then((result) => {
+      let profileId = result.rows[0].id;
+      const values = newProfile.groups.map((groupId) => [profileId, groupId]);
+
+      if (values.length > 0) {
+        let sql = format('INSERT INTO profile_group VALUES %L', values);
+        return pool.query(sql);
+      }
+    });
 };
 
 const deleteProfile = (profileId) => {
-	return pool
-    .query("DELETE FROM profile_group WHERE profile_id = $1", [profileId])
+  return pool
+    .query('DELETE FROM profile_group WHERE profile_id = $1', [profileId])
     .then(() => {
-      return pool.query("DELETE FROM profiles WHERE id = $1", [profileId]);
+      return pool.query('DELETE FROM profiles WHERE id = $1', [profileId]);
     });
-}
-
+};
 
 const getProfileById = (id) => {
-	return pool
-		.query("select profiles.*, coalesce(array_agg(group_name) filter (where profile_group.profile_id is not null), '{}') as groups\
+  return pool
+    .query(
+      "select profiles.*, coalesce(array_agg(group_name) filter (where profile_group.profile_id is not null), '{}') as groups\
 			from profiles\
 			left join profile_group\
 			   on profile_group.profile_id = profiles.id\
 			left join groups\
 			   on profile_group.group_id = groups.id\
 			where profiles.id = $1\
-			group by profiles.id;", 
-				[id])
-		.then((result) => result.rows[0]);
+			group by profiles.id;",
+      [id]
+    )
+    .then((result) => result.rows[0]);
 };
 
 const getAllNationalities = () => {
-	return pool.query("SELECT * FROM nationalities").then((result) => result.rows);
+  return pool
+    .query('SELECT * FROM nationalities')
+    .then((result) => result.rows);
 };
 
 const getAllGroups = () => {
-	return pool.query("select id, group_name, coalesce(array_agg(profile_id) filter (where profile_group.profile_id is not null), '{}') members \
+  return pool
+    .query(
+      "select id, group_name, coalesce(array_agg(profile_id) filter (where profile_group.profile_id is not null), '{}') members \
 					   from groups \
 					   left join profile_group \
 				    	on groups.id = group_id \
-					   group by (id) order by id;")
-		.then((result) => result.rows);
+					   group by (id) order by id;"
+    )
+    .then((result) => result.rows);
 };
 
 module.exports = {
-	getAllProfiles,
-	getProfileById,
-	createProfile,
-	deleteProfile,
-	getAllNationalities,
-	getAllGroups,
+  getAllProfiles,
+  getProfileById,
+  createProfile,
+  deleteProfile,
+  getAllNationalities,
+  getAllGroups,
 };
